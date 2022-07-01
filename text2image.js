06d50550e2e021b3ce1ac87171a2206c245a26b2
getfun <gaifan@hwith.com>
2022-07-01 17:46:23 +0800

    修复js

--- text2image.js
+++ text2image.js
@@ -1,13 +1,7 @@
 
-
-
 main();
 
 function main() {
-    console.log("hello js!")
-
-    var node = document.getElementById('mybody')
-
     readTextFile('blogJsonFile.txt');
 }
 
@@ -22,7 +16,6 @@ function readTextFile(file)
             if(rawFile.status === 200 || rawFile.status == 0)
             {
                 var allText = rawFile.responseText;
-                // alert(allText);
                 handleJsonStr(allText);
             }
         }
@@ -32,28 +25,19 @@ function readTextFile(file)
 
 function handleJsonStr(str) {
     var arr = str.split("\n");
-    // var json = JSON.parse(str);
-    console.log(arr.length);
-
-
-    // for (i = 0; i < json.length; i++) {
-    //     var data = json[i].object.data
-    //     saveOneImage(data)
-    // }
-
-    // var node = document.getElementById('mybody')
-
-    // domtoimage.toPng(node, {width: 800, height: 1200})
-    //     .then(function(dataUrl) {
-    //         var link = document.createElement('a')
-    //         link.download = "all.png"
-    //         link.href = dataUrl
-    //         link.click()
-    //     })
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i].length <= 2) {
+            continue
+        }
+        var json = JSON.parse(arr[i]);
+        for (j = 0; j < json.length; j++) {
+            var data = json[j].object.data
+            appendOne(data)
+        }
+    }
 }
 
-
-function saveOneImage(data) {
+function appendOne(data) {
     var id = data.id
     var title = data.title
     var time = data.first_shared_at
@@ -63,24 +47,13 @@ function saveOneImage(data) {
 
     var h1 = document.createElement('h4')
     var h2 = document.createElement('h5')
-    h1.innerHTML = title
     h2.innerHTML = time
-    
-    console.log(h1.offsetWidth)
 
     node.appendChild(h1)
     node.appendChild(h2)
-}
 
-function requestMyJianshuBlog() {
-    var url = "https://www.jianshu.com/asimov/users/slug/6740854c6174/public_notes?order_by=top&page=7"
-
-}
-
-function requestPage() {
-    var url = "https://www.jianshu.com/asimov/users/slug/6740854c6174/public_notes?order_by=top&page=7"
-    var xhr = new XMLHttpRequest()
-    xhr.open("GET", url, false)
-    xhr.send(null)
-    console.log(xhr.responseText)
+    var a = document.createElement('a')
+    a.innerHTML = title
+    a.href = 'https://www.jianshu.com/p/' + slug
+    h1.appendChild(a)
 }
