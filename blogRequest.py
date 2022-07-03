4f3b48b5c1ae678aa25c3e481ba9dccd5c5741b1
getfun <gaifan@hwith.com>
2022-07-03 09:44:08 +0800

    python生成图片

--- blogRequest.py
+++ blogRequest.py
@@ -1,11 +1,13 @@
+from hashlib import new
 import requests
 import json
 import os
+from PIL import Image, ImageFont, ImageDraw
 
-fpath = 'blogJsonFile.txt'
+jsonPath = 'blogJsonFile.txt'
 
 def requestSaveFile():
-    f = open(fpath, 'w')
+    f = open(jsonPath, 'w')
     headers = {
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"    
     }
@@ -28,14 +30,23 @@ def requestSaveFile():
         f.close()
 
 def parseFile():
-    f = open(fpath, 'r')
+    if os.path.exists('images') == False:
+        os.mkdir('images')
+    f = open(jsonPath, 'r')
     for line in f:
         jo = json.loads(line)
         if (len(jo) == 0):
             break
         for obj in jo:
             data = obj['object']['data']
+            id = data['id']
+            title = data['title']
             print(data['title'])
+
+            fpath = 'images/%s.png' % id
+            if os.path.exists(fpath) == False:
+                createImage(fpath, title)
+
         # print(jo[0]['object']['data'])
 
 # url = 'https://www.jianshu.com/asimov/users/slug/6740854c6174/public_notes?order_by=shared_at&page=10'
@@ -44,7 +55,22 @@ def parseFile():
 # print(jo)
 # print(jo[0]['object']['data'])
 
-if os.path.exists(fpath):
+
+def createImage(fpath, text):
+    fontSize = 30
+    fontPath = '/Library/Fonts/Arial Unicode.ttf'
+    font = ImageFont.truetype(fontPath, fontSize)
+    textSize = font.getsize(text)
+    
+    im = Image.new("RGB", textSize, (255,255,255))
+    dr = ImageDraw.Draw(im)
+
+    dr.text((0, 0), text, font=font, fill='#000000')
+    im.save(fpath)
+
+
+
+if os.path.exists(jsonPath):
     parseFile()
 else:
     requestSaveFile()
